e4959aee0c68dd2f2062b73b80047af96eaac9be
LI_XE <hyangeun312@gmail.com>
2021-05-13 00:39:06 -0500

    Main Content completed

--- portfolio/portfolio/src/App.js
+++ portfolio/portfolio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import MainContent from './components/Maincontent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* Navbar */}
       <Navbar />
       {/* MainContent */}
+      <MainContent/>
       {/* Projects */}
     </div>
   );
---
+++ portfolio/portfolio/src/components/Maincontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContent = () => {
+    return (
+        <div>
+            <h4>This is about me!</h4>
+            <p>The best Lorem Ipsum Generator in all the sea! Heave this scurvy copyfiller fer yar next adventure and cajol yar clients into walking the plank with ev'ry layout! Configure above, then get yer pirate ipsum...own the high seas, arg!</p>
+        </div>
+    )
+}
+
+export default MainContent;
