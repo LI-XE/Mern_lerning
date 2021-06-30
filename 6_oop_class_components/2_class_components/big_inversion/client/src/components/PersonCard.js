2aa476a41bdaecf0a0765cae846f2d73656b1bf7
LI_XE <hyangeun312@gmail.com>
2021-06-30 00:56:04 -0500

    putting it together assignment

--- 6_oop_class_components/2_class_components/big_inversion/client/src/components/PersonCard.js
+++ 6_oop_class_components/2_class_components/big_inversion/client/src/components/PersonCard.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 class PersonCard extends Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            ageUp: this.props.age,
+        }
+    }
     render(){
+        const { firstName, lastName, hairColor } = this.props;
         return(
             <div>
-                <h1>{ this.props.lastName }, { this.props.firstName }</h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: { this.props.hairColor }</p>
+                <h1>{ lastName }, { firstName }</h1>
+                <p>Age: { this.state.ageUp }</p>
+                <p>Hair Color: { hairColor }</p>
+                <button onClick={ () => this.setState({ageUp: this.state.ageUp + 1})}>Birthday Button for { firstName } { lastName }</button>
             </div>
         )
     }
